1931f88dfeaf0de37ca0961f6faf9dec293db0dc
robert <rsprat@gmail.com>
2020-09-26 18:43:45 +0200

    -

--- gsensors.js
+++ gsensors.js
@@ -6,26 +6,59 @@ const absoluteOrientation = new AbsoluteOrientationSensor({ frequency: 60, refer
   // The ThingsBoard telemetry URL
   let at = document.getElementById("at").value;
   let url = "https://demo.thingsboard.io/api/v1/" + at + "/telemetry";
+  let chart = null;
+function createChart()
+{
+  var ctx = document.getElementById('myChart').getContext('2d');
+  chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+  
+      // The data for our dataset
+      data: {
+          labels: [],
+          datasets: [{
+              label: 'X',
+              borderColor: 'rgb(0, 0, 255)',
+              data: []
+          },
+          {
+            label: 'Y',
+            borderColor: 'rgb(255, 0, 0)',
+            data: []
+        },
+        {
+          label: 'Z',
+          borderColor: 'rgb(0, 255, 0)',
+          data: []
+      }
+        
+        ]
+      },
+  
+      // Configuration options go here
+      options: {}
+  });
 
-function startAmbientLightSensor(){
-  if ( 'AmbientLightSensor' in window ) {
-    const sensor = new AmbientLightSensor();
-    sensor.onreading = () => {
-      console.log('Current light level:', sensor.illuminance);
-      document.getElementById("light").innerHTML = sensor.illuminance;
-    };
-    sensor.onerror = (event) => {
-      console.log(event.error.name, event.error.message);
-      showError(event.error.name+": "+event.error.message)
-    };
-    sensor.start();
-  }else
-  {
-    showError("No AmbientLightSensor")
-  }
 }
+function addData(label, data) {
+  let maxLength = 100;
+  chart.data.labels.push(label);
+  
+  chart.data.datasets[0].data.push(data[0]);
+  
+  chart.data.datasets[1].data.push(data[1]);
+  chart.data.datasets[2].data.push(data[2]);
 
-
+  if(chart.data.labels.length > maxLength)
+  {
+    chart.data.labels.shift();
+    chart.data.datasets[0].data.shift();
+    chart.data.datasets[1].data.shift();
+    chart.data.datasets[2].data.shift();
+  }  
+  chart.update();
+}
 function startAccelerationSensor()
 {
   
@@ -45,7 +78,7 @@ var accelerometer = new LinearAccelerationSensor({ frequency: 1 });
    document.getElementById("x").innerHTML = x;
    document.getElementById("y").innerHTML = y;
    document.getElementById("z").innerHTML = z;
-
+   addData(new Date().toISOString().split("T")[1],[x,y,z]);
    // The JSON objects sent to ThingsBoard
    let telemetry = { x: x, y: y, z: z };
    let activity = setActivity(x, y, z);
@@ -73,9 +106,8 @@ function showError(e)
 
 function start() {
 
-  startAmbientLightSensor();
+  createChart();
   startAccelerationSensor();
- 
 
 }
 
